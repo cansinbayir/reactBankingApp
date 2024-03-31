cfdc99a43f5b31cbf83022abd4c78eef8f2bf4db
cansinbayir <cansinb@alumni.sabanciuniv.edu>
2024-04-01 01:32:46 +0300

    2-UserContext, pages and routes created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { UserContext } from "./context/UserContext";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DashBoard from "./pages/DashBoard";
+import Accounts from "./pages/Accounts";
+import Transfer from "./pages/TransferMoney";
+import AddAccount from "./pages/AddAccount";
+import AccountDetails from "./pages/AccountDetails";
+import TransferCompleted from "./pages/TransferCompleted";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(null); // Initialize user as null
+
+  const data = { user, setUser };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <UserContext.Provider value={data}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/accounts" element={<Accounts />} />
+          <Route path="/add-account" element={<AddAccount />} />
+          <Route path="/transfer" element={<Transfer />} />
+          <Route path="/transfer-completed" element={<TransferCompleted />} />
+          <Route
+            path="/account-details/:accountId"
+            element={<AccountDetails />}
+          />
+        </Routes>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
---
+++ src/context/UserContext.js
@@ -0,0 +1,7 @@
+// UserContext.js
+
+import React, { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export { UserContext };
